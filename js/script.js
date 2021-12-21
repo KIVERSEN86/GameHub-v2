e5a7bc33c0deaf6e7a9158de66f5bf58d884dcbc
KIVERSEN86 <73189395+KIVERSEN86@users.noreply.github.com>
2021-12-21 19:34:59 +0100

    Coding product.html

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ hamburgerMenu.onclick = function () {
     event.target.classList.toggle("rotate90");
 }
 
+const button = document.querySelector(".cta-cart");
+
+button.onClick = function () {
+    console.log(event);
+}
